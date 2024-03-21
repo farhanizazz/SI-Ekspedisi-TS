0a9449203a1d46b46f534fa06d19c2dbd4ea596a
Farhan izzaz <farhanizzaz@gmail.com>
2024-03-21 08:01:07 +0700

    mengganti warna mutasi jika mengurangi maka akan merah

--- src/views/admin/Rekening/MutasiRekening.svelte
+++ src/views/admin/Rekening/MutasiRekening.svelte
@@ -1,4 +1,5 @@
 <script>
+	import CardTableMutasi from './../../../notusComponents/Cards/CardTableMutasi.svelte';
   import CardTable from "../../../notusComponents/Cards/CardTable.svelte";
   import { getCookie } from "svelte-cookie";
   import { mainUrl } from "../../../environment";
@@ -9,7 +10,7 @@
 
   export let id;
   let data = [];
-  const heading = ["ID", "Tanggal pembayaran", "Nominal", "Keterangan"];
+  const heading = ["ID", "Tanggal pembayaran", "Nominal","Jenis Transaksi", "Keterangan", "Pembuat"];
 
   function fetchData() {
     fetch(`${mainUrl}/api/getProfile`, {
@@ -32,9 +33,24 @@
                 return e.master_rekening_id == id;
               });
               res.data.forEach((e) => {
+                switch (e.jenis_transaksi) {
+                  case "order":
+                    e.jenis_transaksi = "Pembayaran Order";
+                    e.nominal = `+${e.nominal}`;
+                    break;
+                  case "jual":
+                    e.jenis_transaksi = "Pembayaran Penjualan";
+                    e.nominal = `-${e.nominal}`;
+                    break;
+                  case "uang_jalan":
+                    e.jenis_transaksi = "Pembayaran Uang Jalan";
+                    e.nominal = `-${e.nominal}`;
+                    break;
+                } 
                 delete e.created_at;
                 delete e.updated_at;
                 delete e.master_rekening_id;
+                delete e.created_by;
                 delete e.transaksi_order_id;
                 if (e.keterangan == null) e.keterangan = "Tidak ada keterangan";
               });
@@ -49,8 +65,9 @@
 
 <div class="flex flex-wrap mt-4">
   <div class="w-full mb-12 px-4">
-      <CardTable
+      <CardTableMutasi
         tableHeading={heading}
+        addData={false}
         href="/admin/rekening/mutasi"
         deleteApi={`${mainUrl}/api/master/rekening/mutasi/`}
         heading="Data Rekening"
