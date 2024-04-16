3323ff350e3418ea83c63502f44a40f8d7925d94
Farhan izzaz <farhanizzaz@gmail.com>
2024-04-16 10:30:16 +0700

    Update deleteCookie.js

--- src/helper/deleteCookie.js
+++ src/helper/deleteCookie.js
@@ -1,7 +1,8 @@
 export function deleteCookie(name) {
-    let cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+  let cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+    console.log("cookie", cookie);
     cookie += "; path=/";
-    cookie += `; domain=https://sisteminformasi.pancurmas.com/`;
+    cookie += `; domain=sisteminformasi.pancurmas.com`;
     cookie += "; secure";
     document.cookie = cookie;
   }
