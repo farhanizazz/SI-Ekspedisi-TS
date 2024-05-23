be480f3e926b6a149f7912c9c2b995457f94e080
Farhan izzaz <farhanizzaz@gmail.com>
2024-05-24 00:54:02 +0700

    added add laporan transaksi and view the nota

---
+++ src/views/laporan/Pengeluaran/details/LaporanServisDetail.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import CardTable from "../../../../notusComponents/Cards/CardTable.svelte";
+  import { getCookie } from "svelte-cookie";
+  import { mainUrl } from "../../../../environment";
+  import { Router, Route } from "svelte-routing";
+  import { onMount } from "svelte";
+  import CardTableMutasi from "../../../../notusComponents/Cards/CardTableMutasi.svelte";
+  import type { LaporanServis, NotaBeliItem } from "../models/LaporanModel";
+
+  export let id: number;
+  let data: NotaBeliItem[] = [];
+  const heading = [
+    "ID",
+    "Nama Barang",
+    "Harga",
+    "Jumlah",
+  ];
+
+  function fetchData() {
+    fetch(`${mainUrl}/api/getProfile`, {
+      headers: {
+        Authorization: `bearer ${getCookie("token")}`,
+      },
+    }).then((res) => {
+      res.json().then((res) => {
+        console.log(res);
+        if (res.data.role.akses.master_rekening.view != true) {
+          navigate("/admin/dashboard");
+        } else {
+          fetch(`${mainUrl}/api/laporan/servis/${id}`, {
+            headers: {
+              Authorization: `bearer ${getCookie("token")}`,
+            },
+          }).then((res) => {
+            res.json().then((res) => {
+              res.data.nota_beli_items.forEach((e: NotaBeliItem) => {
+                delete e.created_at;
+                delete e.updated_at;
+                delete e.servis_id;
+              });
+              data = res.data.nota_beli_items;
+              console.log(data);
+            });
+          });
+        }
+      });
+    });
+  }
+</script>
+
+<div class="flex flex-wrap mt-4">
+  <div class="w-full mb-12 px-4">
+    <CardTableMutasi
+      tableHeading={heading}
+      addData={false}
+      href="/admin/rekening/mutasi"
+      deleteApi={`${mainUrl}/api/master/rekening/mutasi/`}
+      heading="Data Laporan Servis"
+      {data}
+      onLoad={fetchData}
+    />
+  </div>
+</div>
